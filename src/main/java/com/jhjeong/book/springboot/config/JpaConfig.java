7bfbb3a44f65ed193a903552d16155ac121decdd
jaehanjeong <jhjeong@nzero.kr>
2020-06-02 11:21:10 +0900

    RDS 실서버 적용

--- src/main/java/com/jhjeong/book/springboot/config/JpaConfig.java
+++ src/main/java/com/jhjeong/book/springboot/config/JpaConfig.java
@@ -1,7 +1,7 @@
 package com.jhjeong.book.springboot.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing // JPA Auditing 활성화
