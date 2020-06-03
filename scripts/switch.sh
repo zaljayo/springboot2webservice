b787663a3781c8563604855a512ccfc9d8e5b35d
jaehanjeong <jhjeong@nzero.kr>
2020-06-03 17:17:59 +0900

    무중단배포테스트3

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -12,7 +12,7 @@ function switch_proxy() {
     #하나의 문장을 만들어 파이프라인(|) 으로 넘겨주기 위해 echo 사용
     #엔진엑스가 변경할 프록시 주소 생성, 홑따옴표 사용
     #앞에서 넘겨준 문장을 service-url.inc에 덮어씀
-    echo 'set \$service_url http://127.0.0.1:${IDLE_PORT};' | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> 엔진엑스 Reload"
     sudo service nginx reload
