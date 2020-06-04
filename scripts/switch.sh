3dffcb620b8f4e42e86608c674654c67dbd26e3e
jaehanjeong <jhjeong@nzero.kr>
2020-06-04 14:38:31 +0900

    무중단배포테스트4

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -10,7 +10,9 @@ function switch_proxy() {
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
     #하나의 문장을 만들어 파이프라인(|) 으로 넘겨주기 위해 echo 사용
-    #엔진엑스가 변경할 프록시 주소 생성, 홑따옴표 사용
+    #쌍따옴표 (") : 변수의 실제 값을 출력
+    #홀따옴표 (') : 변수명을 그대로 출력
+    #여기서는 쌍따옴표 사용해야함
     #앞에서 넘겨준 문장을 service-url.inc에 덮어씀
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
